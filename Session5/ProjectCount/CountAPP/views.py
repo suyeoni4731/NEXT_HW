e4a311589151a8857b7e97e4bf1a8916d06073e9
suyeoni4731 <https4731@naver.com>
2021-04-08 01:55:26 +0900

    django

--- Session5/ProjectCount/CountAPP/views.py
+++ Session5/ProjectCount/CountAPP/views.py
@@ -8,9 +8,12 @@ def result(request):
     text = request.POST['text']
     total_len = len(text)
     no_blank_len = len(text.replace(" ", ""))
+    words = len(text.split(' '))
 
     return render(request, 'result.html', {
         'text': text,
-        'total_len': total_len,
-        'no_blank_len': no_blank_len,
-    })
+        'total_len': total_len, 
+        'no_blank_len': no_blank_len, 
+        'words' : words,
+        })
+
